28a53b223b577d02178071e62468473d60e41621
Salem <salem.zakkar@unifi.solutions>
2026-05-06 09:26:27 +0300

    -- init

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -27,13 +27,20 @@ class AppRoutes {
   static List<String> tabs = [HomePage.path, MapPage.path, SettingsPage.path];
   static List<RouteBase> routes = [
     ShellRoute(
-      // pageBuilder: (context , state , child) => NoTransitionPage(child: child),
       builder: (context, state, child) => NavPage(child: child),
       routes: [
-        GoRoute(builder: (context, state) => HomePage(), path: HomePage.path),
-        GoRoute(builder: (context, state) => MapPage(), path: MapPage.path),
         GoRoute(
-          builder: (context, state) => SettingsPage(),
+          // builder: (context, state) => HomePage(),
+          path: HomePage.path,
+          pageBuilder: (_, state) => NoTransitionPage(child: HomePage()),
+        ),
+        GoRoute(
+          // builder: (context, state) => MapPage(),
+          path: MapPage.path,
+          pageBuilder: (_, state) => NoTransitionPage(child: MapPage()),
+        ),
+        GoRoute(
+          pageBuilder: (_, state) => NoTransitionPage(child: SettingsPage()),
           path: SettingsPage.path,
         ),
       ],
